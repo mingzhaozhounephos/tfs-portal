d7608d19511fd1bd8a3b4605b323ef1c8224f6a1
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-06-01 12:13:17 +0800

    remove empty lines

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,5 +1,3 @@
-
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -11,56 +9,17 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-
 COMMENT ON SCHEMA "public" IS 'standard public schema';
 
-
-
 CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
-
-
-
-
-
-
 CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
-
-
-
-
-
-
 CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
-
-
-
-
-
-
 CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
-
-
-
-
-
-
 CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
-
-
-
-
-
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
-
-
-
-
 SET default_tablespace = '';
-
 SET default_table_access_method = "heap";
-
 
 CREATE TABLE IF NOT EXISTS "public"."users" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
@@ -70,9 +29,7 @@ CREATE TABLE IF NOT EXISTS "public"."users" (
     "full_name" "text"
 );
 
-
 ALTER TABLE "public"."users" OWNER TO "postgres";
-
 
 CREATE TABLE IF NOT EXISTS "public"."users_videos" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
@@ -89,7 +46,6 @@ CREATE TABLE IF NOT EXISTS "public"."users_videos" (
 
 ALTER TABLE "public"."users_videos" OWNER TO "postgres";
 
-
 CREATE TABLE IF NOT EXISTS "public"."videos" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
@@ -102,340 +58,72 @@ CREATE TABLE IF NOT EXISTS "public"."videos" (
     "is_annual_renewal" boolean DEFAULT false NOT NULL
 );
 
-
 ALTER TABLE "public"."videos" OWNER TO "postgres";
-
 
 ALTER TABLE ONLY "public"."users_videos"
     ADD CONSTRAINT "user_videos_pkey" PRIMARY KEY ("id");
 
-
-
 ALTER TABLE ONLY "public"."users"
     ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
-
-
 
 ALTER TABLE ONLY "public"."videos"
     ADD CONSTRAINT "videos_pkey" PRIMARY KEY ("id");
 
-
-
 ALTER TABLE ONLY "public"."users_videos"
     ADD CONSTRAINT "users_videos_user_fkey" FOREIGN KEY ("user") REFERENCES "public"."users"("id") ON DELETE SET NULL;
-
-
 
 ALTER TABLE ONLY "public"."users_videos"
     ADD CONSTRAINT "users_videos_video_fkey" FOREIGN KEY ("video") REFERENCES "public"."videos"("id");
 
-
-
 ALTER TABLE ONLY "public"."videos"
     ADD CONSTRAINT "videos_admin_user_id_fkey" FOREIGN KEY ("admin_user_id") REFERENCES "public"."users"("id") ON DELETE SET NULL;
 
-
-
 CREATE POLICY "Authenticated can access users" ON "public"."users" USING (("auth"."role"() = 'authenticated'::"text"));
-
-
 
 CREATE POLICY "Authenticated can access users_videos" ON "public"."users_videos" USING (("auth"."role"() = 'authenticated'::"text"));
 
-
-
 CREATE POLICY "Authenticated can access videos" ON "public"."videos" USING (("auth"."role"() = 'authenticated'::"text"));
-
-
 
 ALTER TABLE "public"."users" ENABLE ROW LEVEL SECURITY;
 
-
 ALTER TABLE "public"."users_videos" ENABLE ROW LEVEL SECURITY;
-
 
 ALTER TABLE "public"."videos" ENABLE ROW LEVEL SECURITY;
 
-
-
-
 ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
 
-
-
-
-
-
 ALTER PUBLICATION "supabase_realtime" ADD TABLE ONLY "public"."users_videos";
-
-
 
 GRANT USAGE ON SCHEMA "public" TO "postgres";
 GRANT USAGE ON SCHEMA "public" TO "anon";
 GRANT USAGE ON SCHEMA "public" TO "authenticated";
 GRANT USAGE ON SCHEMA "public" TO "service_role";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 GRANT ALL ON TABLE "public"."users" TO "anon";
 GRANT ALL ON TABLE "public"."users" TO "authenticated";
 GRANT ALL ON TABLE "public"."users" TO "service_role";
-
-
 
 GRANT ALL ON TABLE "public"."users_videos" TO "anon";
 GRANT ALL ON TABLE "public"."users_videos" TO "authenticated";
 GRANT ALL ON TABLE "public"."users_videos" TO "service_role";
 
-
-
 GRANT ALL ON TABLE "public"."videos" TO "anon";
 GRANT ALL ON TABLE "public"."videos" TO "authenticated";
 GRANT ALL ON TABLE "public"."videos" TO "service_role";
-
-
-
-
-
-
-
-
 
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
 
-
-
-
-
-
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
 
-
-
-
-
-
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 RESET ALL;
